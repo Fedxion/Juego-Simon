c54d3011b1b0ff54d35a290af20f5d2fe7739b09
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-08-12 21:34:55 +0200

    funcion secuencia

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-let level = 0;
+let nivel = 0;
 let started = false;
+let coloresBotones = ["red", "blue","green","yellow"];
+let patronDeJuego = [];
+let patronDeUsuario = [];
 
 
 
 $(document).keydown(function (tecla) {
     if (tecla.keyCode === 65 && started === false ){
-            $("#level-title").text("Level " + level);
-
+            $("#level-title").text("Level " + nivel);
+            siguienteSecuencia();
             started = true
          }
         });
 
 
 
+  function siguienteSecuencia(){
+    patronDeUsuario = [];
+    nivel++;
+    $("#level-title").text("Nivel " + nivel)
+    let numeroRandom = Math.floor(Math.random() * 4);
+    let colorElegidoRandom = coloresBotones[numeroRandom];
+    patronDeJuego.push(colorElegidoRandom);
+   
 
-
+}
