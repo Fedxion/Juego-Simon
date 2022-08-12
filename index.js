dfff9d9c48322848e1fe23295b20531ee5525fd1
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-08-12 18:32:57 +0200

    started with A

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ let started = false;
 $(document).keydown(function (tecla) {
     if (tecla.keyCode === 65 && started === false ){
             $("#level-title").text("Level " + level);
+
             started = true
          }
         });
+
+
+
+
+
